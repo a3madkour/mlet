0fc05c9db889bf7a8d1a087e1bef3b4a7ff4e9bc
Abdel-Rahman Madkour <a3madkour@gmail.com>
2018-03-01 14:23:59 -0500

    attempting to move the stuff to client directory

---
+++ client/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <h1>MLET App!</h1>
+    <div class="container-fluid"">
+      <div class="row full-width-row">
+        <div class="col-2">
+	      <Toolbar/>
+        </div>
+        <div class="col-10">
+          <Dashboard/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Toolbar from './components/Toolbar.vue'
+import Dashboard from './components/dashboard_components/Dashboard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Toolbar,
+    Dashboard
+  }
+}
+
+</script>
+
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+body {
+  margin: 0px;
+  padding: 0px;
+  min-width:900px;
+  font: 14px "Lucida Grande", Helvetica, Arial, sans-serif;
+}
+
+h1 {
+  text-align: center;
+}
+
+h2 {
+  text-align: center;
+}
+
+.full-width-row {
+  overflow-x: hidden;
+}
+
+.full-width-row > div {
+  margin-left: -15px;
+  margin-right: -15px;
+}
+</style>
---
+++ client/src/components/dashboard_components/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h2 class='text-left'>Notifications</h2>
+        <DashNotificationsTable/>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-6">
+        <h2 class='text-left'>Experiments</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+
+      <div class="col-4">
+        <h2 class='text-left'>Projects</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+
+      <div class="col-2">
+        <h2 class='text-left'>System Info</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DashNotificationsTable from './DashNotificationsTable.vue'
+
+export default {
+  components: {
+    DashNotificationsTable
+  }
+}
+
+</script>
+
+<style lang="scss">
+@import '../../../node_modules/bootstrap/scss/bootstrap.scss';
+</style>

1113b35a13fd86d6b125e3e31535d8367bbdde19
ColinKohler <colink78@gmail.com>
2018-02-28 23:28:22 -0500

    Added bootstrap4 to the package and added some more junk for the dashboard.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
     <h1>MLET App!</h1>
-	<Toolbar/>
+    <div class="container-fluid"">
+      <div class="row full-width-row">
+        <div class="col-2">
+	      <Toolbar/>
+        </div>
+        <div class="col-10">
+          <Dashboard/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 import Toolbar from './components/Toolbar.vue'
+import Dashboard from './components/Dashboard.vue'
 
 export default {
   name: 'app',
   components: {
-    Toolbar
+    Toolbar,
+    Dashboard
   }
 }
 
 </script>
 
 <style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
 
 body {
   margin: 0px;
   padding: 0px;
+  min-width:900px;
   font: 14px "Lucida Grande", Helvetica, Arial, sans-serif;
 }
 
@@ -30,4 +42,16 @@ h1 {
   text-align: center;
 }
 
+h2 {
+  text-align: center;
+}
+
+.full-width-row {
+  overflow-x: hidden;
+}
+
+.full-width-row > div {
+  margin-left: -15px;
+  margin-right: -15px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { TableComponent, TableColumn } from 'vue-table-component';
 import 'vue-awesome/icons'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+
+  components: {
+	TableColumn,
+    TableComponent,
+  },
 })

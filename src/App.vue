1dba9aef20dbf85dccdfeef6ebd4c5080bd64e95
ColinKohler <colink78@gmail.com>
2018-02-28 20:48:05 -0500

    Changed to webpack to get Vue working.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>MLET App!</h1>
+	<Toolbar/>
+  </div>
+</template>
+
+<script>
+
+import Toolbar from './components/Toolbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Toolbar
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+body {
+  margin: 0px;
+  padding: 0px;
+  font: 14px "Lucida Grande", Helvetica, Arial, sans-serif;
+}
+
+h1 {
+  text-align: center;
+}
+
+</style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="toolbar">
+    <div class="top">
+      <a class="active" href="#" title="Dashboard"><icon name="fa-home"></icon><span>Dashboard</span></a>
+      <a href="#" title="Projects"><icon name="folder-open"></icon><span>Projects</span></a>
+      <a href="#" title="Experiments"><i class="fa fa-flask"></i><span>Experiments</span></a>
+    </div>
+    <div class="bottom">
+      <a href="#" title="Search"><i class="fa fa-search"></i><span>Search</span></a>
+      <a href="#" title="Settings"><i class="fa fa-cog"></i><span>Settings</span></a>
+    </div>
+  </div>
+</template>
+
+<style>
+.toolbar {
+  width: 125px;
+  height: 100%;
+  position: fixed;
+  margin: 0px;
+  padding: 0px;
+  background-color: #555;
+}
+
+.toolbar .top {
+  position: fixed;
+  top: 0px;
+  width: inherit;
+}
+
+.toolbar .bottom {
+  position: fixed;
+  bottom: 0px;
+  width: inherit;
+}
+
+.toolbar a {
+  display: block;
+  text-align: center;
+  padding: 16px;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 36px;
+}
+
+.toolbar a span {
+  color: white;
+  font-size: 10px;
+  padding: 5px 0 10px 0;
+  display: block;
+  text-transform: uppercase;
+  letter-spacing: 1px; 
+}
+
+.toolbar a:hover {
+  background-color: #000;
+}
+
+.toolbar .active {
+  background-color: #4CAF50;
+}
+</style>

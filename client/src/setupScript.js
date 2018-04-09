daddc7abe61f473235133bd641b8dd7becf564ca
Abdel-Rahman Madkour <a3madkour@gmail.com>
2018-04-09 23:02:47 +0000

    finished setup script

--- client/src/setupScript.js
+++ client/src/setupScript.js
@@ -1,11 +1,28 @@
 var exec = require('child_process').exec, child;
 var request = require('request');
+var baseUrl = 'localhost:8081'
+ function addUser(postData,context){
+            var clientServerOptions = {
+                uri: 'http://'+baseUrl+''+context,
+                body: JSON.stringify(postData),
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            request(clientServerOptions, function (error, response) {
+                console.log(error,response.body);
+              
+                return;
+            });
+        }
 
-// child = exec('mongo mlet --eval "db.dropDatabase()"',
-//     function (error, stdout, stderr) {
-//         console.log('stdout: ' + stdout);
-//         console.log('stderr: ' + stderr);
-//         if (error !== null) {
-//              console.log('exec error: ' + error);
-//         }
-//     });
+child = exec('mongo mlet --eval "db.dropDatabase()"',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+        addUser({name : "User", password: "Iamagoodboi"}, "/users")
+    });
